1feb430ec5ebbbafad3df5dd9e3827de69aa6ff3
Sergio García <47102779+Sergiogarciaruiz@users.noreply.github.com>
2020-02-04 11:11:20 +0100

    Gestion de coches AndriodStudio

---
+++ 2º DAM/PMDM/Examen_Tr1_PMDM/app/src/main/java/com/example/examen_tr1_pmdm/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.example.examen_tr1_pmdm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(getApplicationContext(), HomeScreen.class);
+                startActivity(i);
+                finish();
+            }
+        }, 3000);
+    }
+}
